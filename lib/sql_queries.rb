22badeb661f9706e40e70b08311ec1bc8e1f4cf4
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-28 04:19:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -5,8 +5,10 @@
 # end
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
+require 'pry'
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
+binding.pry
 "SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id=pledges.project_id ORDER BY (projects.title)"
 end
 
